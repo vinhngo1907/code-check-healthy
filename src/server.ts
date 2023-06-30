339661b901ae1b36f66b49725d29d5f004480e3a
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-06-30 17:10:16 +0700

    app.ts

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import { app } from "./app";
+
+const port = process.env.PORT || 3000;
+
+const isProduction = process.env.IS_PRODUCTION === "true";
+
+if (isProduction) {
+    app.set("env", "production");
+}
+
+app.listen(port, () => {
+    console.log(`App started on http://localhost:${port}`);
+});
