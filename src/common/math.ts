417aca495a220dc9dea201cca735219ebfabbbbc
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-06-30 17:05:14 +0700

    first commit

---
+++ src/common/math.ts
@@ -0,0 +1,8 @@
+const isPositiveInteger = (value: number): boolean => {
+    if (Number.isInteger(Number(value)) && value >= 0) {
+        return true;
+    }
+    return false;
+}
+
+export { isPositiveInteger };
