6cc956150fde55183acbd610fe81cf61992365fa
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-07-01 11:09:09 +0700

    service

--- src/video/service.ts
+++ src/video/service.ts
@@ -1,9 +1,11 @@
-// import {extract} from "./processor";
+import { extractFrame } from "./processor";
 
 import { ExtractFrameParams } from "./params";
 
 export default class VideoProcessingService {
-    public static async extracFrameInBase64 ():Promise <ExtractFrameParams>{
-        
+    public static async extracFrameInBase64(params: ExtractFrameParams): Promise<string> {
+        const buffers: Buffer = await extractFrame(params);
+        const base64 = buffers.toString("base64");
+        return base64;
     }
 }
